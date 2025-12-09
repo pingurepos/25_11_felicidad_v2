e568700c3bd1a359fe0f6291bf56b159e546399c
Pelishka <pelishk@gmail.com>
2025-12-09 11:56:51 -0600

    pablo

--- analisis_25_11/02_analisis_datos_anteriores/01_analisis.R
+++ analisis_25_11/02_analisis_datos_anteriores/01_analisis.R
@@ -6,7 +6,9 @@ library(dplyr)
 library(psych)
 library(tidyr)
 library(ggplot2)
+library(stringr)
 
+mar_original <- par("mar")
 
 datos <- read_xlsx('../../respaldo_analisis_anterior/22_12_ipn_analisis_felicidad/Prueba piloto nueva medición Comprom15o 20_12.xls')
 
@@ -63,8 +65,8 @@ datos <- datos %>%
   mutate(
     across(everything(), ~ordered(.))
   ) %>% 
-  filter(if_all(42:74, ~ !is.na(.))) %>% 
-  mutate(across(42:74, ~as.numeric(.)))
+  filter(if_all(42:74, ~ !is.na(.))) 
+
 
 
 names(datos)[42:74] <- nombres_cortos
@@ -76,6 +78,9 @@ datos %>%
 datos %>% 
   dplyr::select(42:74) %>% 
   str
+
+
+datos <- datos %>% mutate(across(42:74, ~as.numeric(.)))
 
 datos <- datos %>% 
   clean_names
@@ -94,34 +99,181 @@ ggplot(resumen, aes(x = value, group = name, color = name)) +
   theme_minimal()
 
 
+# clusterización jerárquica ---------------
 
 
-poly <- polychoric(datos %>% dplyr::select(42:74) %>% data.frame)
+library(dendextend)
+
+poly <- polychoric(datos %>% dplyr::select(42:74) %>% data.frame %>% setNames(str_replace_all(names(.),'_',' ')))
 
 r <- poly$rho
 
-fa.parallel(
-  r,
-  n.obs = nrow(datos %>% 
-                 dplyr::select(42:74) ),
-  fm    = "ml",   # ML factor analysis
-  fa    = "fa"
+distancia <- as.dist(1-r)
+
+hc <- hclust(distancia, method = 'ward.D2')
+
+
+dendograma <- as.dendrogram(hc)
+
+dendograma <- dendograma %>% 
+  color_branches(k = 15) %>% 
+  color_labels (k = 15)
+
+
+par(mar = c(1,1,1,30))
+plot(dendograma, horiz = T, axes = F)
+
+
+rect.dendrogram(
+  dendograma,
+  k = 7,
+  horiz = T,
+  border = 'black',xpd = T
+  
 )
 
 
-k<- 7
+clusters <- cutree(hc, k = 7)
 
-
-efa1 <- fa(
-  r,
-  nfactors = k,
-  n.obs    = 1568,
-  fm       = "ml",       # or "minres" if ML has issues
-  rotate   = "oblimin"
+info_clusters <- data.frame(
+  variable = names(clusters),
+  cluster = clusters %>% unname
 )
 
+# evaluación de información aportada --------------------
 
-fa.diagram(
-  efa1,digits = 2,simple = F
+library(mirt)
+library(purrr)
+
+modelo <- mirt(datos %>% dplyr::select(42:74) %>% data.frame %>% setNames(str_replace_all(names(.),'_',' ')), model = 1, itemtype = 'graded',
+               technical = list(
+                 NCYCLES = 3000  
+       
+               ))
+
+datos$score <- fscores(modelo, method = 'EAP') %>% as.vector
+
+mean(datos$score)
+
+ggplot(datos, aes(x = score)) +
+  geom_histogram() + 
+  geom_density()
+
+
+plot(modelo, type = 'info')
+
+
+informacion <- list()
+
+for(i in 1:33){
+  
+  item <- extract.item(modelo, i)
+  theta_grid <- matrix(seq(-2, 2, length.out = 81), ncol = 1)
+  informacion[[i]] <- mirt::iteminfo(item, Theta = theta_grid) 
+  informacion[[i]] <- mean(informacion[[i]])
+  
+}
+
+resumen_info <- data.frame(
+  variable = datos %>% dplyr::select(42:74) %>% data.frame %>% setNames(str_replace_all(names(.),'_',' ')) %>% names,
+  informacion_promedio = reduce(informacion,c)
+) %>% 
+  mutate(
+    informacion_proporcion = informacion_promedio / sum(informacion_promedio)
+  ) %>% 
+  arrange(desc(informacion_proporcion)) %>% 
+  mutate(
+    variable = factor(variable, levels = unique(variable))
+  )
+
+
+ggplot(resumen_info, aes(x = variable, y = informacion_proporcion)) +
+  geom_bar(stat = 'identity') +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+
+plot(modelo,type = 'info')
+plot(modelo,type = 'rxx')
+# plot(modelo,type = 'infocontour')
+plot(modelo,type = 'SE')
+plot(modelo,type = 'infotrace')
+# plot(modelo,type = 'infoSE')
+plot(modelo,type = 'trace')
+# plot(modelo,type = 'intemscore')
+plot(modelo,type = 'score')
+# plot(modelo,type = 'scorecontour')
+plot(modelo,type = 'posteriorTheta')
+# plot(modelo,type = 'EPAsum')
+
+
+# información por item --------------------
+
+library(ggrepel)
+
+grid <- matrix(seq(-2, 2, length.out = 81), ncol = 1)
+
+informacion <- list()
+
+for(i in 1:33){
+  
+  item <- extract.item(modelo, i)
+  theta_grid <- matrix(seq(-2, 2, length.out = 81), ncol = 1)
+  informacion[[i]] <- mirt::iteminfo(item, Theta = theta_grid) 
+  
+}
+
+informacion_items_2 <- bind_cols(informacion)
+
+names(informacion_items_2) <- nombres_cortos
+
+info_total <- colSums(informacion_items_2)
+
+centro_info <- colSums(informacion_items_2 * grid) / info_total
+
+centro_info <- data.frame(
+  variable = names(centro_info),
+  centro_info
 )
+
+ggplot(centro_info, aes(x = 1, y = centro_info, label = variable)) +
+  geom_point()+
+  geom_label_repel(max.overlaps = Inf) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+
+
+# GRÁFICA FINAL
+
+resumen_info <- resumen_info %>% 
+  mutate(variable = as.character(variable))
+
+centro_info <- centro_info %>% 
+  arrange(variable)
+
+resumen_info <- resumen_info %>% 
+  arrange(variable)
+
+info_clusters <- info_clusters %>% 
+  arrange(variable)
+
+resumen_final <- bind_cols(
+  centro_info,
+  resumen_info,
+  info_clusters
+) %>% 
+  clean_names %>% 
+  mutate(cluster = as.factor(cluster))
+  
+
+
+ggplot(resumen_final, aes(x = centro_info, y = informacion_proporcion, label = variable_1, color = cluster)) +
+  geom_point() +
+  geom_label_repel(max.overlaps = Inf) +
+  theme_minimal() +
+  labs(
+    x = 'centro de información',
+    y = 'proporción de información aportada'
+  )
+
 
